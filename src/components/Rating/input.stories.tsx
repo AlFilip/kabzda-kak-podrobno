ec20332f51ddbba2ace2004caaad2f18d99eafae
Alex <t9371100211@gmail.com>
2021-09-09 12:59:36 +0300

    Made refactoring

--- src/components/Rating/input.stories.tsx
+++ src/components/Rating/input.stories.tsx
@@ -1,6 +1,6 @@
 // Button.stories.ts | Button.stories.tsx
 
-import React, {ChangeEventHandler, useRef, useState} from 'react'
+import React, {ChangeEvent, ChangeEventHandler, useRef, useState} from 'react'
 
 import {Meta, Story} from '@storybook/react';
 import {action} from "@storybook/addon-actions";
@@ -9,6 +9,7 @@ export default {
     // component: OnOff,
     title: 'example',
 } as Meta;
+
 
 export const GetValueOfInputByButtonPress: Story = () => {
     const [value, setValue] = useState<string>('')
@@ -25,29 +26,69 @@ export const GetValueOfInputByButtonPress: Story = () => {
     </>
 }
 
+
 export const ControlledInput: Story<{ value: string }> = (args) => {
     const [value, setValue] = useState<string>('')
+
     const onChange: ChangeEventHandler<HTMLInputElement> = (e): void => {
         action('')
         setValue(e.currentTarget.value)
     }
+
     return <>
         <input value={value} onChange={onChange} type="text"/>
         Current Value =&gt; {value}
     </>
 }
 
+
 export const ControlledCheckbox: Story<{ on: boolean }> = (args) => {
     const [on, setOn] = useState<boolean>(false)
 
+    const onChange = () => setOn(!on)
+
     return (
         <div>
-            <input checked={on} onChange={() => setOn(!on)} type="checkbox"/>
-            =&gt; {on ? 'Checked': 'Not Checked'}
+            <input checked={on} onChange={onChange} type="checkbox"/>
+            =&gt; {on ? 'Checked' : 'Not Checked'}
         </div>
     )
 }
 
-export const ControlledSelect: Story = () => {
-    return <input type="select"/>
+
+type optionType = {
+    value: string
+    title: string
+}
+type ControlledSelectType = {
+    optionsList: Array<optionType>
+}
+
+export const ControlledSelect: Story<ControlledSelectType> = (args) => {
+    const [parentValue, setParentValue] = useState<string>('default')
+
+    const onChange = (e:ChangeEvent<HTMLSelectElement>): void => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    const options = args.optionsList.map(o => <option value={o.value}>{o.title}</option>)
+
+    return (
+        <div>
+            <select value={parentValue} onChange={onChange}>
+                {options}
+            </select>
+            <span>
+                Current Value =&gt; {parentValue}
+            </span>
+        </div>
+    )
+}
+ControlledSelect.args = {
+    optionsList: [
+        {value: 'default', title: 'none',},
+        {value: '1', title: 'Minsk',},
+        {value: '2', title: 'Moscow',},
+        {value: '3', title: 'Kiev',},
+    ]
 }
